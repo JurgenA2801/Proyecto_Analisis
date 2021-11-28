93845b52290c99607fd2cd4e127fabd7cff6e0fb
Jurgen Anton <jurgen.jam28@gmail.com>
2021-11-27 20:11:10 -0600

    Se crearon las tablas roles y usuarios

---
+++ database/migrations/2021_11_23_204202_create_usuarios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('idRol');
+            $table->string('nombreUsuario');
+            $table->string('password');
+            $table->foreign('idRol')->references('id')->on('roles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
